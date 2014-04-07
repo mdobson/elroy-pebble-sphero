69ebc553c97f9b116761dcdf1fee48f97867ca7a
Matt Dobson <mdobson4@gmail.com>
2014-04-07 14:30:35 -0400

    Added sphero call to random color changing.

--- app.js
+++ app.js
@@ -2,21 +2,26 @@ var PebbleSpheroApp = module.exports = function(){
   this.name = 'pebblesphero';
 }
 
+var cb = function() {};
+
 PebbleSpheroApp.prototype.init = function(elroy){
-  elroy.observe('type="Sphero"').subscribe(function(sphero) {
+  elroy.observe('type="sphero"').subscribe(function(sphero) {
     elroy.expose(sphero);
     elroy.observe('type="smartwatch"').subscribe(function(pebble) {
       elroy.expose(pebble);
+      setInterval(function(){
+        sphero.call('random-color', cb);
+      }, 3000);
       pebble.on('top-button', function(){
-        sphero.call('right');
+        sphero.call('right', cb);
       });
 
       pebble.on('bottom-button', function() {
-        sphero.call('left');
+        sphero.call('left', cb);
       });
 
       pebble.on('select-button', function() {
-        sphero.call('move', 5);
+        sphero.call('move', 5, cb);
       });
     });
   });
