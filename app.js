301252102785219cb13befb2d5e7017e95736058
Matt Dobson <mdobson4@gmail.com>
2014-05-01 11:10:44 -0400

    Updates to zetta-sphero app

--- app.js
+++ app.js
@@ -5,24 +5,29 @@ var PebbleSpheroApp = module.exports = function(){
 var cb = function() {};
 
 PebbleSpheroApp.prototype.init = function(elroy){
-  elroy.observe('type="sphero"').subscribe(function(sphero) {
-    elroy.expose(sphero);
-    elroy.observe('type="smartwatch"').subscribe(function(pebble) {
-      elroy.expose(pebble);
-      setInterval(function(){
-        sphero.call('random-color', cb);
-      }, 3000);
-      pebble.on('top-button', function(){
-        sphero.call('right', cb);
-      });
+  elroy.observe('type="sphero"')
+  .zip(elroy.observer('type="smartwatch"'))
+  .subscribe(function(devices) {
+    var sphero = devices[0];
+    var pebble = devices[1];
+    
+    setInterval(function(){
+      sphero.call('random-color', cb);
+    }, 3000);
 
-      pebble.on('bottom-button', function() {
-        sphero.call('left', cb);
-      });
-
-      pebble.on('select-button', function() {
-        sphero.call('move', 5, cb);
-      });
+    pebble.on('top-button', function(){
+      sphero.call('right', cb);
     });
+
+    pebble.on('bottom-button', function() {
+      sphero.call('left', cb);
+    });
+
+    pebble.on('select-button', function() {
+      sphero.call('move', 5, cb);
+    });
+
+    elroy.expose(pebble);
+    elroy.expose(sphero);
   });
 };
